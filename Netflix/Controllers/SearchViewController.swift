96dabcfb22f5210b2077f7817d64b5ba41bd94dc
Mabast O. Hamadamin <mabastu@gmail.com>
2023-08-12 09:39:00 +0300

    (2:24:27) Creating Upcoming TableView inside Upcoming Tab

--- Netflix/Controllers/SearchViewController.swift
+++ Netflix/Controllers/SearchViewController.swift
@@ -11,8 +11,10 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemPink
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        view.backgroundColor = .systemBackground
     }
 
 }
