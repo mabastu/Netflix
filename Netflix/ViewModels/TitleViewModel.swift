1dc3ebbcae6c1dd98166393c76ba3a3fb253ddb6
Mabast O. Hamadamin <mabastu@gmail.com>
2023-08-12 09:38:18 +0300

    (2:24:27) Creating Upcoming TableView inside Upcoming Tab

---
+++ Netflix/ViewModels/TitleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TitleViewModel.swift
+//  Netflix
+//
+//  Created by Mabast on 12/08/2023.
+//
+
+import Foundation
---
+++ Netflix/Views/TitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleTableViewCell.swift
+//  Netflix
+//
+//  Created by Mabast on 10/08/2023.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
